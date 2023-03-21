543114d15cd46ca2721433f8d97e811032277347
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-03-21 08:03:27 +0300

    Started FinalVerificationTask

--- FinalVerificationTask/Program.cs
+++ FinalVerificationTask/Program.cs
@@ -2,6 +2,14 @@
 {
     internal class Program
     {
+        static void GetArray (string[] array)
+        {
+            string[]newArray = new string[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if ()
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
