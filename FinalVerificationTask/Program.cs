32d51e6d347b3dc00c42071283f3733605fe9700
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-03-21 08:10:58 +0300

    Create metod GetNewArray

--- FinalVerificationTask/Program.cs
+++ FinalVerificationTask/Program.cs
@@ -2,12 +2,15 @@
 {
     internal class Program
     {
-        static void GetArray (string[] array)
+        static void GetNewArray (string[] array)
         {
             string[]newArray = new string[array.Length];
             for (int i = 0; i < array.Length; i++)
             {
-                if ()
+                if (array[i].Length <= 3)
+                {
+                    newArray[i] = array[i];                    
+                } 
             }
         }
         static void Main(string[] args)
